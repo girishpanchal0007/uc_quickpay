7811042b241046eaebf871c7d9d6ccd81f5d192c
Girish <girish@krishaweb.com>
2016-09-14 15:03:12 +0530

    quickpay card_token

--- js/uc_quickpay.js
+++ js/uc_quickpay.js
@@ -1,26 +1,28 @@
 jQuery(document).ready(function($) {
-
-    jQuery('#edit-continue').click(function(event) {
-        /* Act on the event */
-        var ExMonth = jQuery('#edit-panes-payment-details-cc-exp-month').val();
-        var ExYear = jQuery('#edit-panes-payment-details-cc-exp-year').val().slice("-2");
-        if(ExMonth < 10){
-            jQuery('#cc-date-year').val('0' + ExMonth + '/' + ExYear);
-        }
-        else{
-            jQuery('#cc-date-year').val(ExMonth + '/' + ExYear);  
-        }
-    });
+    jQuery('input#edit-panes-payment-details-cc-number').removeAttr('name');
+    // jQuery('select#edit-panes-payment-details-cc-exp-month').removeAttr('name');
+    // jQuery('select#edit-panes-payment-details-cc-exp-year').removeAttr('name');
+    jQuery('input#cc-date-year').removeAttr('name');
+    jQuery('input#edit-panes-payment-details-cc-cvv').removeAttr('name');
+    // jQuery('#edit-continue').click(function(event) {
+    //     /* Act on the event */
+    //     var ExMonth = jQuery('#edit-panes-payment-details-cc-exp-month').val();
+    //     var ExYear = jQuery('#edit-panes-payment-details-cc-exp-year').val().slice("-2");
+    //     if(ExMonth < 10){
+    //         jQuery('#cc-date-year').val('0' + ExMonth + '/' + ExYear);
+    //     }
+    //     else{
+    //         jQuery('#cc-date-year').val(ExMonth + '/' + ExYear);  
+    //     }
+    // });
 });
 
 jQuery(document).ready(function($) {
-    //var MerchantID = drupalSettings.uc_quickpay.merchant_id;
-    //var AgreementID = drupalSettings.uc_quickpay.agreement_id;
-    QuickPay.Embedded.Form(jQuery('.uc-cart-checkout-form #edit-continue'), {      
+    QuickPay.Embedded.Form(jQuery('.uc-cart-checkout-form'), {   
         merchant_id: 21882,
         agreement_id: 79491,
         brandChanged: function(brand) {
-            jQuery('.form-item-panes-payment-details-cc-number').html(brand);
+            //jQuery('.form-item-panes-payment-details-cc-number').html(brand);
         },
         beforeCreateToken: function(form) {
         	jQuery('input.error').removeClass('error');
@@ -35,9 +37,6 @@ jQuery(document).ready(function($) {
           	    alert(source + ': ' + message);
         	}
         	jQuery('.uc-cart-checkout-form #edit-continue').html('Pay');
-        },
-        success: function(form, token){
-            console.log(token);
         }
     });
 });
